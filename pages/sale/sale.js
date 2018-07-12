bb3d4dd4d06b01223998c6a19e6111bb23528137
hnksweb <469657170@qq.com>
2018-07-12 23:58:46 +0800

    First commit

---
+++ pages/sale/sale.js
@@ -0,0 +1,86 @@
+// pages/sale/sale.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sale:[{
+      key:"标准车报价",
+      name:"标准车报价",
+      imgUrl:"/image/logo.png"
+    }, {
+      key: "非标车报价",
+      name: "非标车报价",
+      imgUrl: "/image/logo.png"
+      }, {
+        key: "网络告知",
+        name: "网络告知",
+        imgUrl: "/image/logo.png"
+    }, {
+        key: "合格证查询",
+        name: "合格证查询",
+      imgUrl: "/image/logo.png"
+      }, {
+        key: "生产进度查询",
+        name: "生产进度查询",
+        imgUrl: "/image/logo.png"
+    },]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
